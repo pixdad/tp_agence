8d19a8cec43393943253a7ecc3cbf7176ea5f558
Pierre Pelé <pierre.pele@uavia.eu>
2015-02-17 12:41:33 +0100

    Formulaire de connexion.

---
+++ front/vue/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Agence - Login</title>
+</head>
+<body>
+
+<h1>Connexion</h1>
+<div>
+	<form action="../script/login.php" method="post">
+		<fieldset>
+			<legend>Connexion</legend>
+			<input type="text" name="login"	placeholder="Saisissez votre login ...">
+			<input type="password" name="passwd" placeholder="Votre mot de passe ...">
+			<input type="submit" value="Se connecter">
+		</fieldset>
+	</form>
+</div>
+	
+</body>
+</html>
