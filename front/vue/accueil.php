235d82525e710493601f059d6e6656bb28dd33b7
pixdad <david.oudart@gmail.com>
2015-02-17 21:06:17 +0100

    style

--- front/vue/accueil.php
+++ front/vue/accueil.php
@@ -8,9 +8,16 @@
 	<?php include '../../include/vue/head.php'; ?>
 </head>
 <body>
-	<header>
-		<h1>Travel'INT</h1>
+	<header id="one_page">
+		<h1 class="h0 ta-center">Travel'INT<br/>Agency</h1>
 	</header>
 	
+
+	<script>
+	$(function() {
+		$('#one_page').height($(window).height());
+		alert($(window).height())
+	});
+	</script>
 </body>
 </html>
--- include/head.php
+++ include/head.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" href="../../css/style.css">
 <link rel="stylesheet" href="../../css/form.css">
 <link rel="stylesheet" href="../../css/propriete.css">
+<script src="../../js/jquery.min.js"></script>
