2213b02b352c17d294443e26cb4d98424095a65e
pixdad <david.oudart@gmail.com>
2015-02-19 13:00:19 +0100

    back-office

--- back/vue/_afficherCircuit.php
+++ back/vue/_afficherCircuit.php
@@ -5,21 +5,33 @@ include_once '../../include/script/secur_back.php';
 <table class="full ta-center bg-fond-sombre">
 	<tr style="border-bottom:1px solid white">
 		<td>Description</td>
-		<td>Date de départ</td>
-		<td>Date de réservation</td>
-		<td>Prix</td>
+		<td>Ville de départ</td>
+		<td>Pays de départ</td>
+		<td>Ville d'arrivée</td>
+		<td>Durée en jours</td>
+		<td>Modifier</td>
 		<td>Supprimer</td>
 	</tr>
 
 	<?php 
 
-		$sql = 'SELECT description, dateDepart, villeDepart, villeArrivee, paysDepart, R.programmationID, dateReservation, prix
-				FROM RESERVATION R, CIRCUITPROGRAMME CP, CIRCUIT C
-				WHERE R.programmationID = CP.programmationID AND CP.circuitID = C.circuitID AND R.clientID = ?';
-		$req = $bdd->prepare($sql);
-		$req->execute(array($id));
+		$sql = 'SELECT * FROM CIRCUIT';
+		$req = $bdd->query($sql);
 		if (!$req){
 			echo "\nEreur :\n";
 			print_r($req->errorInfo());
 		}
 		while($donnees = $req->fetch()) { ?>
+
+		<tr>
+			<td><?=$donnees['description']?></td>
+			<td><?=$donnees['villeDepart']?></td>
+			<td><?=$donnees['paysDepart']?></td>
+			<td><?=$donnees['villeArrivee']?></td>
+			<td><?=$donnees['dureeCircuit']?></td>
+			<td><form action=""><input type="hidden"><input type="submit" value="Modifier"></form></td>
+			<td><form action=""><input type="hidden"><input type="submit" value="Supprimer"></form></td>
+		</tr>
+
+		<?php } ?>
+</table>
