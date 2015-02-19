514843996010459f6468b18b9dbd7959381a9f6b
pixdad <david.oudart@gmail.com>
2015-02-19 11:51:02 +0100

    avis terminés

--- front/script/afficherAvis_scr.php
+++ front/script/afficherAvis_scr.php
@@ -1,7 +1,7 @@
 <?php 
 include '../../include/script/secur_front.php';
 
-$template_html = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Affichage des avis - Travel\'INT Agency</title></head><body>';
+$template_html = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Affichage des avis - Travel\'INT Agency</title><link rel="stylesheet" href="../../css/style.css"></head><body>';
 
 echo $template_html;
 $req = "";
