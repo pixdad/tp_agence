13de030e93294f87b23137f87c49659c6b16f78e
Pierre Pelé <pierre.pele@uavia.eu>
2015-02-19 11:28:00 +0100

    Corrections.

--- front/script/afficherAvis_scr.php
+++ front/script/afficherAvis_scr.php
@@ -4,6 +4,7 @@ include '../../include/script/secur_front.php';
 $template_html = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Affichage des avis - Travel\'INT Agency</title></head><body>';
 
 echo $template_html;
+$req = "";
 
 if (isset($_GET['id']))
 {
